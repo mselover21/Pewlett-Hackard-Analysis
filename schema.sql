03f773378adc49e07c61588b28402eb25877a66d
mselover21 <mselover21@gmail.com>
2022-07-28 16:11:29 -0600

    working on modules

--- schema.sql
+++ schema.sql
@@ -62,3 +62,39 @@ CREATE TABLE titles (
 
 -- Getting number of columns in departments tabel
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM dept_manager;
+SELECT * FROM dept_emp;
+SELECT * FROM titles;
+
+-- Finding employee DOB between 1952 and 155
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+-- Find employee DOB 1952, 1953, 1954, 1955 respectively
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Narrowing it down by hire date 
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
