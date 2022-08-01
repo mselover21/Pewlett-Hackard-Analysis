068d0debc49c7633dc43a8a9b3184942500244ff
mselover21 <mselover21@gmail.com>
2022-07-31 19:53:25 -0700

    Done with deliverable 2

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -45,3 +45,19 @@ ORDER BY count(title) DESC;
 
 -- Check the table
 SELECT * FROM retiring_titles;
+
+-- Deliverable 2
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+		e.first_name,
+		e.last_name,
+		e.birth_date,
+		de.from_date,
+		de.to_date,
+		t.title
+-- INTO mentorship_elegibility
+FROM employees as e
+INNER JOIN dept_emp as de ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1962-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
